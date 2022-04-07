0973ae74a3e8f5012e5add545d27a625d958f012
RHariSankar <rhari.s7@gmail.com>
2022-04-07 21:52:05 +0530

    [in-progress] updating timeline data

--- src/pages/timeline.jsx
+++ src/pages/timeline.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Chrono } from "react-chrono";
 import data from "./timelineData";
+import ChildFriendlyRoundedIcon from '@mui/icons-material/ChildFriendlyRounded';
 
 export default function Timeline() {
     return (
@@ -9,6 +10,25 @@ export default function Timeline() {
             <Chrono items={data} mode="VERTICAL_ALTERNATING" enableOutline>
                 <div></div>
                 <div></div>
+                <div>
+                    <ul>
+                        <li>CGPA - 10.0</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>Class 12 - 95.2%</li>
+                        <li>JEE AIR - 15138</li>
+                        <li>Kerala Entrance Rank - 433</li>
+                        <li>Joined Collge of Engineering, Trivandrum - B.Tech In Computer Science</li>
+                    </ul>
+                </div>
+                <div className="chrono-icons">
+                    <img src="birthday.svg" alt="image2" />
+                    <img src="books.svg" alt="image2" />
+                    <img src="books.svg" alt="image2" />
+                    <img src="books.svg" alt="image2" />
+                </div>
             </Chrono>
         </div>
 
--- src/pages/timelineData.jsx
+++ src/pages/timelineData.jsx
@@ -6,24 +6,19 @@ const items = [
         "Born, 22 November, Allapuzha, Kerala",
     },
     {
-      title: "2004-2012",
+      title: "2004-2013",
       cardTitle: "Primary Education",
       cardSubtitle: `Classes 1 to 9, VSSC Central School, Thumba, Trivandrum`,
     },
     {
-      title: "June 1941",
-      cardTitle: "Operation Barbarossa",
-      cardSubtitle: `A column of Red Army prisoners taken during the first days of the German invasion`,
-      cardDetailedText: `Since the 1920s, Hitler had seen Russia, with its immense natural resources, as the principal target for conquest and expansion. It would provide, he believed, the necessary ‘Lebensraum’, or living space, for the German people. And by conquering Russia, Hitler would also destroy the “Jewish pestilential creed of Bolshevism”. His non-aggression pact with Stalin in August 1939 he regarded as a mere temporary expedient.
-        Barely a month after the fall of France, and while the Battle of Britain was being fought, Hitler started planning for the Blitzkrieg campaign against Russia, which began on 22 June 1941. Despite repeated warnings, Stalin was taken by surprise, and for the first few months the Germans achieved spectacular victories, capturing huge swathes of land and hundreds of thousands of prisoners. But they failed to take Moscow or Leningrad before winter set in.
-        On 5/6 December, the Red Army launched a counter-offensive which removed the immediate threat to the Soviet capital. It also brought the German high command to the brink of a catastrophic military crisis. Hitler stepped in and took personal command. His intervention was decisive and he later boasted, “That we overcame this winter and are today in a position again to proceed victoriously… is solely attributable to the bravery of the soldiers at the front and my firm will to hold out…”`
+      title: "2014",
+      cardTitle: "Class 10",
+      cardSubtitle: `AISSCE board exam`,
     },
     {
-      title: "7 December 1941",
-      cardTitle: "Pearl Harbor",
-      cardSubtitle: `The destroyer USS Shaw explodes in dry dock after being hit by Japanese aircraft`,
-      cardDetailedText: `After Japan’s occupation of French Indo-China in July 1941, US President Franklin D Roosevelt, followed by Britain and the Netherlands, ordered the freezing of Japanese assets.
-        Many Japanese now believed that there was no alternative between economic ruin and going to war with the United States and the European colonial powers. In October 1941, a hardline government under General Hideki Tojo came to power, and preparations were made to deliver a devastating blow against the Americans.`
+      title: "2016",
+      cardTitle: "Class 12",
+      cardSubtitle: `AISCE board exams, entrance exams and college admissions`,
     },
     {
       title: "15 February 1942",
